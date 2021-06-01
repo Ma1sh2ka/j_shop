60c9b6b8bada54076367933a75ec4f9124414737
Ma1sh2ka <56895622+Ma1sh2ka@users.noreply.github.com>
2021-06-01 04:36:56 +0300

    commit0106

--- src/proj/urls.py
+++ src/proj/urls.py
@@ -16,11 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from cities import views as cities_views
+# from cities import views as cities_views
 from spravochniki import views as spravochniki_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('<code>/', cities_views.cities),
-    # path('', cities_views.home)
+    # path('', cities_views.home),
+    path('author/<int:author_id>/', spravochniki_views.author, name='author'),
+    path('authors/', spravochniki_views.author_list, name='author-list'),
+    path('series/<int:series_id>/', spravochniki_views.series, name='series'),
+    path('seriess/', spravochniki_views.series_list, name='series-list'),
+    path('genre/<int:genre_id>/', spravochniki_views.genre, name='genre'),
+    path('genres/', spravochniki_views.genre_list, name='genre-list'),
+    path('publishing_house/<int:publishing_house_id>/', spravochniki_views.publishing_house, name='publishing_house'),
+    path('publishing_houses/', spravochniki_views.publishing_house_list, name='publishing_house-list'),
+    path('', spravochniki_views.home)
 ]
--- src/spravochniki/views.py
+++ src/spravochniki/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
 
+from . import models
+
 # Create your views here.
+
+def author(request, author_id):
+    author = models.Author.objects.get(pk=author_id)
+    ctx = {
+        'author': author,
+    }
+    return render(request, template_name='author_detail.html', context=ctx)
+
+def author_list(request):
+    author_list = models.Author.objects.all()
+    ctx = {
+        'author_list': author_list,
+    }
+    return render(request, template_name='author_list.html', context=ctx)
+
+
+def series(request, series_id):
+    series = models.Series.objects.get(pk=series_id)
+    ctx = {
+        'series': series,
+    }
+    return render(request, template_name='series_detail.html', context=ctx)
+
+def series_list(request):
+    series_list = models.Series.objects.all()
+    ctx = {
+        'series_list': series_list,
+    }
+    return render(request, template_name='series_list.html', context=ctx)
+
+
+def genre(request, genre_id):
+    genre = models.Genre.objects.get(pk=genre_id)
+    ctx = {
+        'genre': genre,
+    }
+    return render(request, template_name='genre_detail.html', context=ctx)
+
+def genre_list(request):
+    genre_list = models.Genre.objects.all()
+    ctx = {
+        'genre_list': genre_list,
+    }
+    return render(request, template_name='genre_list.html', context=ctx)
+
+def publishing_house(request, publishing_house_id):
+    publishing_house = models.Publishing_house.objects.get(pk=publishing_house_id)
+    ctx = {
+        'publishing_house': publishing_house,
+    }
+    return render(request, template_name='publishing_house_detail.html', context=ctx)
+
+def publishing_house_list(request):
+    publishing_house_list = models.Publishing_house.objects.all()
+    ctx = {
+        'publishing_house_list': publishing_house_list,
+    }
+    return render(request, template_name='publishing_house_list.html', context=ctx)
+
+def home(request):
+    return render(request, template_name='homepage.html', context={})
